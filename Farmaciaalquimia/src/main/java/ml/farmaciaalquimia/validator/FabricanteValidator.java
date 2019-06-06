433d403cf05375eb316df05d49161e9e75814568
eduardotorres <dudutorres00@outlook.com>
2019-06-06 01:54:07 -0300

    incluido o bootstrap

--- Farmaciaalquimia/src/main/java/ml/farmaciaalquimia/validator/FabricanteValidator.java
+++ Farmaciaalquimia/src/main/java/ml/farmaciaalquimia/validator/FabricanteValidator.java
@@ -1,13 +1,18 @@
 package ml.farmaciaalquimia.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import ml.farmaciaalquimia.model.Fabricante;
+import ml.farmaciaalquimia.service.FabricanteService;
 
 @Component
 public class FabricanteValidator implements Validator {
+	
+	@Autowired
+	private FabricanteService fabricanteService;
 
 	@Override
 	public boolean supports(Class<?> aClass) {
