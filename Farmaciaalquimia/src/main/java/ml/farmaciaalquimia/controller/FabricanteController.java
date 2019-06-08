1006183f11817f053243e8ce2b0e765d0ca44037
eduardotorres <dudutorres00@outlook.com>
2019-06-08 00:40:23 -0300

    .

--- Farmaciaalquimia/src/main/java/ml/farmaciaalquimia/controller/FabricanteController.java
+++ Farmaciaalquimia/src/main/java/ml/farmaciaalquimia/controller/FabricanteController.java
@@ -26,46 +26,46 @@ public class FabricanteController {
 	public String listaFabricantes(Model model){
 		List<Fabricante> listaFabricantes = fabricanteService.findAll();
 		model.addAttribute("listaFabricantes", listaFabricantes);
-		return "module/fabricante/index";
+		return "module/fabricantes/index";
 	}
 	
 	@PostMapping("/fabricantes")
 	public String save(@ModelAttribute("fabricanteForm") Fabricante fabricante, BindingResult bindingResult){
 		 if (bindingResult.hasErrors()) {
-	            return "module/fabricante/fabricanteForm";
+	            return "module/fabricante/formFabricante";
 		 }
 		 fabricanteService.save(fabricante);
 		 
-		 return "module/fabricante/index";
+		 return "module/fabricantes/index";
 	}
 	
 	@PostMapping("/fabricantes/update")
 	public String update(@ModelAttribute("fabricanteUpdate") Fabricante fabricante, BindingResult bindingResult){
 		if (bindingResult.hasErrors()) {
-            return "module/fabricante/fabricanteForm";
+            return "module/fabricantes/formFabricante";
 		}
 		fabricanteService.update(fabricante);
 		
-		return "module/fabricante/index";
+		return "module/fabricantes/index";
 	}
 	
 	@GetMapping("/fabricantes/update/{id}")
 	public String update(@PathVariable("id") Long id, Model model){
 		Fabricante fabricante = fabricanteService.findById(id);
 		model.addAttribute("fabricanteUpdate", fabricante);
-		return "module/fabricante/formUpdateFabricante";
+		return "module/fabricantes/formUpdateFabricante";
 }
 	
 	@GetMapping("/fabricantes/delete/{id}")
 	public String delete(@PathVariable("id") Long id){
 		fabricanteService.delete(id);
-		return "redirect:/fabricante";
+		return "redirect:/fabricantes";
 	}
 	
 	@GetMapping("fabricantes/form")
 	public String prepareFabricanteForm(Model model){
 		Fabricante fabricante= new Fabricante();
 		model.addAttribute("fabricanteForm", fabricante);
-		return "module/fabricante/formFabricante";
+		return "module/fabricantes/formFabricante";
 	}
 }
